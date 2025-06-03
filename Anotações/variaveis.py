090bf28c68458a96c71302b09428492e5e2e62f8
Joao Goedert <joaogueda@hotmail.com>
2025-06-03 10:06:10 -0300

    commit com anotações

---
+++ Anotações/variaveis.py
@@ -0,0 +1,26 @@
+# Tipos de variaveis
+
+idade = 15 #Int
+hora = 22.15 #Float
+nome = 'Joao' #Str
+tabelaVerdade = True or False #Bool
+none = None #Vazio, sem valor
+
+# Como printar os seus tipos
+print(type(idade))
+print(type(hora))
+print(type(nome))
+print(type(tabelaVerdade))
+print(type(none))
+
+#Exercicio para fazer
+alunos_presentes = 23
+alunos_faltantes = '17'
+
+soma = int(alunos_faltantes) + alunos_presentes
+print(soma)
+
+#forma de mostrar uma barra invertida(\\)
+print('c:\\User\\Joao')
+#mostrando aspas(\' texto \')
+print('Joao é um grande \'poeta\'')
